e8c95d2262f4f8356963ca4e25cb7f91ad1f81db
widsnoy <ricofx47@outlook.com>
2024-12-03 09:27:33 +0800

    update

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,8 +6,10 @@ mod console;
 
 mod lang_items;
 mod sbi;
+mod logging;
 
 use core::arch::global_asm;
+use log::*;
 
 //use sbi::console_putchar;
 global_asm!(include_str!("entry.asm"));
@@ -15,8 +17,8 @@ global_asm!(include_str!("entry.asm"));
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
     clear_bss();
-    let x:Option<u32> = Some(233u32);
-    print!("{}", x.unwrap());
+    logging::init();
+    space_info();
     loop{}
 }
 fn clear_bss() {
@@ -31,3 +33,45 @@ fn clear_bss() {
     })
 }
 
+pub fn space_info() {
+    unsafe extern "C" {
+        fn stext();
+        fn etext();
+
+        fn srodata();
+        fn erodata();
+
+        fn sdata();
+        fn edata();
+
+        fn boot_stack_lower_bound();
+        fn boot_stack_top();
+
+        fn sbss();
+        fn ebss();
+    }
+
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top_bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
+    // CI autotest success: sbi::shutdown(false)
+    // CI autotest failed : sbi::shutdown(true)
+    sbi::shutdown(false)
+}
