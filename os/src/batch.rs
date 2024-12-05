a14318d0c19d2b38953d739addff73d6c4e0dc10
widsnoy <ricofx47@outlook.com>
2024-12-05 18:18:09 +0800

    update

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -1,16 +1,57 @@
 use crate::sync::UPSafeCell;
+use crate::sbi::shutdown;
+use core::arch::asm;
+use core::ffi::c_ushort;
 use lazy_static::*;
 
 const MAX_APP_NUM: usize = 16;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
 
 struct AppManager {
     num_app: usize,
     current_app: usize,
     app_start: [usize; MAX_APP_NUM + 1],
 }
+impl AppManager {
+    pub fn print_app_info(&self) {
+        println!("[kernel] num_app = {}", self.num_app);
+        for i in 0..self.num_app {
+            println!(
+                "[kernel] app_{} [{:#x}, {:#x})",
+                i,
+                self.app_start[i],
+                self.app_start[i + 1]
+            );
+        }
+    }
+
+    unsafe fn load_app(&self, app_id: usize) {
+        if app_id >= self.num_app {
+            println!("All applications completed!");
+            shutdown(false);
+        }
+        println!("[kernel] Loading app_{}", app_id);
+        unsafe {
+            core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, APP_SIZE_LIMIT).fill(0);
+            let app_src = core::slice::from_raw_parts(self.app_start[app_id] as *const u8, self.app_start[app_id + 1] - self.app_start[app_id]);
+            let app_dst = core::slice::from_raw_parts_mut(APP_BASE_ADDRESS as *mut u8, app_src.len());
+            app_dst.copy_from_slice(app_src);
+            asm!("fence.i");
+        }
+    }
+
+    pub fn get_current_app(&self) -> usize {
+        self.current_app
+    }
+    
+    pub fn move_to_next_app(&mut self) {
+        self.current_app += 1;
+    }
+}
 
 lazy_static! {
-    static ref APP_MANAGER: UPSafeCell<AppManager> = unsafe {
+    static ref  APP_MANAGER: UPSafeCell<AppManager> = unsafe {
         UPSafeCell::new({
             unsafe extern "C" {
                 fn _num_app();
@@ -29,4 +70,22 @@ lazy_static! {
         })
     };
 
+}
+
+pub fn print_app_info() {
+    APP_MANAGER.exclusive_access().print_app_info();
+}
+
+pub fn init() {
+    print_app_info();
+}
+
+pub run_next_app() -> ! {
+    let mut app_manager = APP_MANAGER.exclusive_access();
+    let current_app = app_manager.get_current_app();
+    unsafe {
+        app_manager.load_app(current_app + 1);
+    }
+    app_manager.move_to_next_app();
+    drop(app_manager);
 }
